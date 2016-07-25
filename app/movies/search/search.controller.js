540af801ee87e48f10146bbe5d4dd7cbacc178d5
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-07-25 02:39:53 -0400

    only display an image when there is actual content, also refactored how to get an image.

--- app/movies/search/search.controller.js
+++ app/movies/search/search.controller.js
@@ -2,35 +2,32 @@
   angular.module('search')
     .controller('SearchController', SearchController);
 
-    SearchController.$inject = ['$scope','API_KEY', '$resource', 'IMAGE_URL'];
+    SearchController.$inject = ['$scope','API_KEY', '$resource'];
 
-    function SearchController($scope, API_KEY, $resource, IMAGE_URL) {
+    function SearchController($scope, API_KEY, $resource) {
        console.log('In the search Controller');
 
        $scope.findMovie = function() {
-         console.log('sucessfully called the findMovie function')
          if ($scope.searchContent){
            var input = $scope.searchContent;
            const call = `http://api.themoviedb.org/3/search/movie?${API_KEY}&query=${input}`;
            $resource(call).get().$promise.then(
              someData => {
                var x = someData.results[0];
+
                var title = x.original_title;
                $scope.title = title;
                var overview = x.overview;
                $scope.desc = overview;
-               debugger;
                var shortcut = x.poster_path;
-               var picture = `${IMAGE_URL}${shortcut}`;
-               console.log(picture);
-               $scope.picUrl = picture;
+               console.log(shortcut);
+               $scope.picture = shortcut;
+             });
 
-             })
-
-             $scope.searchContent = '';
+        }
+        else {
+          console.log('please fill out the form');
         }
        }
      }
 }
-// https://api.themoviedb.org/3/movie/550/images?api_key=dfadb7f7c0b6a5358a9ea7f7ed15fb20&query=en&include_image_language=en,null
-// 'http://api.themoviedb.org/3/search/movie?api_key=dfadb7f7c0b6a5358a9ea7f7ed15fb20&query=step%20%brothers'
